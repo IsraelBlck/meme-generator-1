3cf267ab6459d672a53cd56b21d1e2a8196573d6
Israel Onoja <Israelonoja21@gmail.com>
2023-10-26 00:18:56 +0200

    import meme component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import './App.css'
 import Header from '../public/components/Header'
+import Meme from '../public/components/Meme'
 
 function App() {
   return (
     <>
       <Header />
+      <Meme />
     </>
   )
 }
