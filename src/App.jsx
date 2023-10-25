1c03d8c5ea37965600908f8cf001f1216f509b83
Israel Onoja <Israelonoja21@gmail.com>
2023-10-26 00:46:02 +0200

    render meme component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
-import { useState } from 'react'
-import './App.css'
-import Header from '../public/components/Header'
-import Meme from '../public/components/Meme'
+import { useState } from "react";
+import "./App.css";
+import Header from "../public/components/Header";
+import Meme from "../public/components/Meme";
 
 function App() {
   return (
     <>
       <Header />
-      <Meme />
+      <section>
+        <Meme />
+      </section>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
