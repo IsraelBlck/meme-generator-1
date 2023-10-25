39e99520b68e5970f30903702aa23f200ee9cd94
Israel Onoja <Israelonoja21@gmail.com>
2023-10-25 04:30:02 +0200

    update header css

--- public/components/Header.jsx
+++ public/components/Header.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 export default function Header() {
   return (
     <header className='header'>
-        <img className="header--logo" src="../images/Troll Face.png" alt="logio" />
+        <img className="header--logo" src="../images/Troll Face.png" alt="logo" />
         <h3 className='header--title'>Meme Generator</h3>
-        <h5 className='header--description'>React Course - Project 3</h5>
+        <h5 className='header--project'>React Course - Project 3</h5>
     </header>
   )
 }
