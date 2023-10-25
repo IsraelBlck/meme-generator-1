c85f4fcfa98daf7ef6805cffdcaafab3c4e45b8d
Israel Onoja <Israelonoja21@gmail.com>
2023-10-25 02:56:05 +0200

    header component

---
+++ public/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <header className='header'>
+        <img className="header--logo" src="../images/Troll Face.png" alt="logio" />
+        <h3 className='header--title'>Meme Generator</h3>
+        <h5 className='header--description'>React Course - Project 3</h5>
+    </header>
+  )
+}
