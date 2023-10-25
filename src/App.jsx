4c7787416974a97201df9098986a400b89690de0
Israel Onoja <Israelonoja21@gmail.com>
2023-10-25 03:45:00 +0200

    styled header

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import { useState } from 'react'
 import './App.css'
+import Header from '../public/components/Header'
 
 function App() {
   return (
     <>
-      <h1>Hello World</h1>
+      <Header />
     </>
   )
 }
