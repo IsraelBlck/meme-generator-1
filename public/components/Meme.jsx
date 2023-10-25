6da4e5f600fbc915ab3d1a4ec25082dbbcc84bbb
Israel Onoja <Israelonoja21@gmail.com>
2023-10-26 00:18:15 +0200

    update meme

--- public/components/Meme.jsx
+++ public/components/Meme.jsx
@@ -3,7 +3,13 @@ import React from 'react'
 export default function Meme() {
   return (
     <div>
-      
+      <form className='form'>
+        <input type="text" placeholder='Shut up'/>
+        <input type="text" placeholder='and take my money'/>
+      </form>
+      <button> Get a new meme image 🖼</button>
     </div>
   )
 }
+
+
