dadca7fbfdd5b309062c880b151f008b41952b91
Israel Onoja <Israelonoja21@gmail.com>
2023-12-12 04:35:11 +0100

    set meme top text and bottom text

--- public/components/Meme.jsx
+++ public/components/Meme.jsx
@@ -1,43 +1,37 @@
-import React from "react";
+import React from 'react'
 
 export default function Meme() {
-  // set up top text and bottom text
-  const [formData, setFormData] = React.useState({
-    topText: "",
-    bottomText: "",
-    randomImage: "http://i.imgflip.com/1bij.jpg",
-  });
 
-  function handleChange(event) {
-    const { name, value } = event.target;
-    setFormData((prevFormData) => ({
-      ...prevFormData,
-      [name]: value,
-    }));
-  }
-  // 
+  // set up top text and bottom text
+const [memeForm, setMemeForm] = React.useState({
+    topText: '',
+    bottomText: '',
+    randomImage: 'http://i.imgflip.com/1bij.jpg'
+})
+
 
   return (
-    <div className="meme">
-      <div className="form">
-        <input
-          type="text"
-          placeholder="Shut up"
-          className="form--input"
-          name="topText"
-          value={formData.topText}
-          onChange={handleChange}
+    <div className='meme'>
+      <div className='form'>
+        <input 
+        type="text"
+        placeholder='Shut up'
+        className='form--input'
         />
-        <input
-          type="text"
-          placeholder="and take my money"
-          className="form--input"
-          name="bottomText"
-          value={formData.bottomText}
-          onChange={handleChange}
+        <input 
+        type="text"
+        placeholder='and take my money'
+        className='form--input'
         />
-        <button> Get a new meme image 🖼</button>
+      <button> Get a new meme image 🖼</button>
+      </div>
+      <div className="meme--form">
+        <h1 className="meme--text top">Hello</h1>
+        <h1 className="meme--text bottom">How are you</h1>
+        <img className='meme--image' src="http://i.imgflip.com/1bij.jpg" alt="meme images" />
       </div>
     </div>
-  );
+  )
 }
+
+
