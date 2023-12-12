a572e2222f2cedc99847ed2b63c38ff53f169c38
Israel Onoja <Israelonoja21@gmail.com>
2023-12-12 03:56:28 +0100

    update form: topText and bottomText

--- public/components/Meme.jsx
+++ public/components/Meme.jsx
@@ -1,33 +1,43 @@
-import React from 'react'
+import React from "react";
 
 export default function Meme() {
-
   // set up top text and bottom text
-const [meme, setMeme] = React.useState({
-    topText: '',
-    bottomText: '',
-    randomImage: 'http://i.imgflip.com/1bij.jpg'
-})
+  const [formData, setFormData] = React.useState({
+    topText: "",
+    bottomText: "",
+    randomImage: "http://i.imgflip.com/1bij.jpg",
+  });
 
-
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [name]: value,
+    }));
+  }
+  // 
 
   return (
-    <div className='meme'>
-      <div className='form'>
-        <input 
-        type="text"
-        placeholder='Shut up'
-        className='form--input'
+    <div className="meme">
+      <div className="form">
+        <input
+          type="text"
+          placeholder="Shut up"
+          className="form--input"
+          name="topText"
+          value={formData.topText}
+          onChange={handleChange}
         />
-        <input 
-        type="text"
-        placeholder='and take my money'
-        className='form--input'
+        <input
+          type="text"
+          placeholder="and take my money"
+          className="form--input"
+          name="bottomText"
+          value={formData.bottomText}
+          onChange={handleChange}
         />
-      <button> Get a new meme image 🖼</button>
+        <button> Get a new meme image 🖼</button>
       </div>
     </div>
-  )
+  );
 }
-
-
